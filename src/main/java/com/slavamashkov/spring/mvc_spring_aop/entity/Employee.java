7ac32d7c8f45dac8f156b226278407c386edb341
DayDream-21 <vova13158@gmail.com>
2021-09-18 19:49:11 +0300

    Added Lombok in pom.xml and created entity Employee

---
+++ src/main/java/com/slavamashkov/spring/mvc_spring_aop/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.slavamashkov.spring.mvc_spring_aop.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private int salary;
+}
